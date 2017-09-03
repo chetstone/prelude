8dd30cf47665eec2a9ecb237257f2758518de8e1
Chester Wood <chet@dewachen.org>
2017-09-03 13:20:42 -0600

    Syncronize custom.el with crystal

---
+++ personal/custom.el
@@ -0,0 +1,73 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(aw-scope (quote frame))
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" default)))
+ '(dired-mode-hook (quote (dired-extra-startup)))
+ '(fci-rule-color "#383838")
+ '(global-subword-mode t)
+ '(grep-files-aliases
+   (quote
+    (("js" . "*.js *.jsx")
+     ("all" . "* .*")
+     ("el" . "*.el")
+     ("ch" . "*.[ch]")
+     ("c" . "*.c")
+     ("cc" . "*.cc *.cxx *.cpp *.C *.CC *.c++")
+     ("cchh" . "*.cc *.[ch]xx *.[ch]pp *.[CHh] *.CC *.HH *.[ch]++")
+     ("hh" . "*.hxx *.hpp *.[Hh] *.HH *.h++")
+     ("h" . "*.h")
+     ("l" . "[Cc]hange[Ll]og*")
+     ("m" . "[Mm]akefile*")
+     ("tex" . "*.tex")
+     ("texi" . "*.texi")
+     ("asm" . "*.[sS]"))))
+ '(magit-popup-use-prefix-argument (quote disabled))
+ '(package-selected-packages
+   (quote
+    (coffee-mode helm-projectile helm beacon datetime seq zop-to-char prettier-js csv-mode wgrep iedit editorconfig log4j-mode logview json-mode magit swift-mode zenburn-theme yaml-mode web-mode volatile-highlights vkill undo-tree smartrep smartparens scss-mode rainbow-mode projectile ov operate-on-number move-text markdown-mode magit-find-file key-chord js2-mode haml-mode guru-mode groovy-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elisp-slime-nav easy-kill discover-my-major diminish diff-hl browse-kill-ring anzu ack-and-a-half ace-window ace-jump-buffer)))
+ '(prettier-js-command "prettier-emacs")
+ '(projectile-globally-ignored-directories
+   (quote
+    ("docs" "node_modules" ".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "build")))
+ '(projectile-globally-ignored-files (quote ("bundle.js" "main.jsbundle" "TAGS")))
+ '(projectile-mode-line-lighter "Prj")
+ '(safe-local-variable-values (quote ((web-mode-content-type . "jsx"))))
+ '(scss-compile-at-save nil)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(vc-handled-backends (quote (RCS CVS SVN SCCS SRC Bzr Hg Mtn)))
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-enable-auto-quoting nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "dark slate gray")))))
